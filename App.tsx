2a9ed2424cacf24e916855f5032acfb999276b1b
Rama Pramudhita Bhaskara <2241720128@student.polinema.ac.id>
2024-12-18 19:37:34 +0700

    fix : navbar

--- App.tsx
+++ App.tsx
@@ -3,9 +3,11 @@ import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomePage from './src/pages/HomePage';
 import ListPage from './src/pages/ListPage';
-import DetailPage from './src/pages/DetailPage';
+import DetailMoviePage from './src/pages/DetailMoviePage';
 import ProfilePage from './src/pages/ProfilePage';
+import DetailBookPage from './src/pages/DetailBookPage';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -13,34 +15,44 @@ const Tab = createBottomTabNavigator();
 const ListStack = () => (
   <Stack.Navigator>
     <Stack.Screen name="List" component={ListPage} options={{ headerShown: false }} />
-    <Stack.Screen name="Detail" component={DetailPage} options={{ title: 'Detail' }} />
+    <Stack.Screen name="Detail" component={DetailBookPage} options={{ title: 'Detail' }} />
+    <Stack.Screen name="DetailMovie" component={DetailMoviePage} options={{ title: 'Detail' }} />
   </Stack.Navigator>
 );
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          tabBarIcon: ({ color, size }) => {
-            let iconSource;
-
-            if (route.name === 'Home') {
-              iconSource = require('./assets/ico/home.png');
-            } else if (route.name === 'Profile') {
-              iconSource = require('./assets/ico/profile.png');
-            }
-
-            return <Image source={iconSource} style={{ width: size, height: size, tintColor: color }} />;
-          },
-        })}
-      >
-        <Tab.Screen name="Home" component={ListStack} />
-        <Tab.Screen name="Profile" component={ProfilePage} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={MainTabs} options={{ headerShown: false }} />
+        <Stack.Screen name="ListStack" component={ListStack} options={{ headerShown: false }} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
+const MainTabs = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      headerShown: false,
+      tabBarIcon: ({ color, size }) => {
+        let iconSource;
+
+        if (route.name === 'Home') {
+          iconSource = require('./assets/ico/home.png');
+        } else if (route.name === 'Profile') {
+          iconSource = require('./assets/ico/profile.png');
+        }
+
+        return <Image source={iconSource} style={{ width: size, height: size, tintColor: color }} />;
+      },
+      tabBarActiveTintColor: '#FFD700', 
+      tabBarInactiveTintColor: 'gray', 
+    })}
+  >
+    <Tab.Screen name="Home" component={HomePage} />
+    <Tab.Screen name="Profile" component={ProfilePage} />
+  </Tab.Navigator>
+)
 
 export default App;
